39564e76f1a8e27872ccdac7c1df00d9ca473fe2
임준식 <yjs3694@gmail.com>
2020-05-04 22:11:07 +0900

    ApiClient 클래스 작성 완료

--- weather/weather/Network/ApiClient.swift
+++ weather/weather/Network/ApiClient.swift
@@ -14,7 +14,7 @@ import Foundation
 import NetworkExtension
 
 
-// MARK : - request를 위한 interface
+// MARK: - request를 위한 interface
 public protocol NetworkRequest {
     func request(
         _ url: String,
@@ -23,13 +23,33 @@ public protocol NetworkRequest {
 }
 
 
-// MARK : - request 메서드 (공통)
+// MARK: - request 메서드 (공통)
 public class ApiClient: NetworkRequest {
     
     public func request(_ url: String, success: @escaping (Data) -> Void, fail: @escaping (NSError?) -> Void) {
         do {
             // url 생성
-            guard let apiURI = URL(string:"\()
+            guard let apiURI = URL(string:"\(WTUrl.prefixUrl)/\(url)") else {
+                print("Invalid API URL)")
+                return
+            }
+            NSLog("==========> [Request] URL = \(url)\n\n")
+            
+            // REST API 호출
+            let data = try Data(contentsOf: apiURI)
+            
+            // 로그 출력
+            let log = NSString(data: data, encoding: String.Encoding.utf8.rawValue) ?? "" // NSString으로 인코딩
+            NSLog("==========> [Response] Data = \(log)")
+            
+            success(data)
+            
+        } catch let err as NSError {
+            // error 처리
+            NSLog(err.localizedDescription)
+            
+            fail(err)
         }
     }
 }
+
